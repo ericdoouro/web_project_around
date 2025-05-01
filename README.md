8b84fa82e53716661ae4324efa22c2a12cb95139
ericdoouro <ericouro1992@gmail.com>
2025-05-01 16:21:42 -0300

    Correcao de erro dos dados no formulario

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Tripleten web_project_around
+
+Este é um projeto um pouco mais complexo de um perfil de usuário com layout responsivo, desenvolvido com HTML, CSS e JavaScript. O layout se adapta automaticamente a telas menores que 800px utilizando Media Queries.
+
+A página conta com botões funcionais para adicionar pessoas e imagens, cujas ações são implementadas por meio de comandos em JavaScript, proporcionando maior interatividade e usabilidade à interface.
+
+## Tecnologias utilizadas
+
+- HTML
+- CSS (com Media Queries)
+- Java Script
+
+## Funcionalidades
+
+- Estrutura de perfil com foto, nome e profissão
+- Botão de editar e adicionar
+- Responsividade para telas menores que 800px
+- Uso de border-top responsivo que cobre 100% da largura no mobile
+
+## Responsividade
+
+O layout usa Media Queries para reorganizar os elementos quando a largura da tela for menor que 800px, empilhando os elementos verticalmente e centralizando o conteúdo.
+
+## Como usar
+
+1. Clone o repositório:
+   bash
+   git clone https://github.com/ericdoouro/web_project_around.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,16 @@
 const editButton = document.querySelector(".profile__edit");
 const popup = document.querySelector(".popup");
-const closeButton = document.querySelector(".popup__close-button")
+const closeButton = document.querySelector(".popup__close-button");
+
+const inputName = document.getElementById("name");
+const inputAbout = document.getElementById("about");
+
+const profileName = document.querySelector(".profile__info-name");
+const profileProfession = document.querySelector(".profile__info-profession");
 
 editButton.addEventListener("click", () => {
+  inputName.value = profileName.textContent;
+  inputAbout.value = profileProfession.textContent;
   popup.classList.add("popup_opened");
 });
 
@@ -10,9 +18,22 @@ closeButton.addEventListener("click", () => {
   popup.classList.remove("popup_opened");
 });
 
+const form = document.querySelector(".popup__form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  profileName.textContent = inputName.value;
+  profileProfession.textContent = inputAbout.value;
+
+  popup.classList.remove("popup_opened");
+
+  form.reset();
+});
+
 const addButton = document.querySelector(".profile__add");
 const popupAddCard = document.querySelector(".popup-add-card");
-const closeButtonAddCard = document.querySelector(".popup-add-card__close-button")
+const closeButtonAddCard = document.querySelector(".popup-add-card__close-button");
 
 addButton.addEventListener("click", () => {
   popupAddCard.classList.add("popup_opened");
